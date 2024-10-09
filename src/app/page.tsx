d7397804196e4f034e5e3e65b236ef9b877c7448
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-09 02:14:38 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,106 @@
-import Image from "next/image";
+"use client"
+import { SetStateAction, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [messageHi, setMessageHi] = useState('');
+  const [messageHello, setMessageHello] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const messagesHi = [
+    {
+      id: 1,
+      message: "hi there",
+    },
+    {
+      id: 2,
+      message: "good to know.",
+    },
+  ];
+
+  const messagesHello = [
+    {
+      id: 1,
+      message: "hello!",
+    },
+    {
+      id: 2,
+      message: "How are you?",
+    },
+  ];
+
+  const combinedMessages = [...messagesHi, ...messagesHello];
+
+  function handleSubmitLeft(event: { preventDefault: () => void; }) {
+    console.log(messageHi);
+    setMessageHi('');
+  }
+
+  function handleChangeLeft(event: { target: { value: SetStateAction<string>; }; }) {
+    setMessageHi(event.target.value);
+  }
+
+  function handleSubmitRight(event: { preventDefault: () => void; }) {
+    console.log(messageHello);
+    setMessageHello('');
+  }
+
+  function handleChangeRight(event: { target: { value: SetStateAction<string>; }; }) {
+    setMessageHello(event.target.value);
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen bg-black justify-center items-center text-center p-10">
+
+      <div className="basis-1/4 mb-6">
+        <h1 className="font-formulaLight tracking-widest text-5xl text-white">
+          chat with yourself
+        </h1>
+      </div>
+
+      <div className="basis-1/4 font-formulaLight text-center justify-center items-center text-3xl text-white">
+      <div className="flex flex-col items-start space-y-4 my-4 w-full">
+            {combinedMessages.map((msg) => (
+              <div key={msg.id}>
+                <p className={`p-2`}>
+                  {msg.message}
+                </p>
+              </div>
+            ))}
+          </div>
+      </div>
+
+      <div className="flex flex-row flex-1 min-w-full justify-center items-center gap-6">
+        <div className="flex-1 flex flex-col justify-center items-center text-center">
+          <h1 className="font-formulaLight tracking-widest text-8xl text-white">hello</h1>
+          <input
+            className="font-formulaLight tracking-widest text-4xl text-white bg-transparent outline-none p-2"
+            onChange={handleChangeLeft}
+            placeholder="Enter a message here"
+            value={messageHi}
+          />
+          <button
+            className="font-formulaLight tracking-widest text-4xl text-gray-500 hover:text-gray-800 mt-4"
+            onClick={handleSubmitLeft}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            send message
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="flex-1 flex flex-col justify-center items-center text-center">
+          <h1 className="font-formulaLight tracking-widest text-8xl text-white">hi</h1>
+          <input
+            className="font-formulaLight tracking-widest text-4xl text-white bg-transparent outline-none p-2"
+            onChange={handleChangeRight}
+            placeholder="Enter a message here"
+            value={messageHello}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button
+            className="font-formulaLight tracking-widest text-4xl text-gray-500 hover:text-gray-800 mt-4"
+            onClick={handleSubmitRight}
+          >
+            send message
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
